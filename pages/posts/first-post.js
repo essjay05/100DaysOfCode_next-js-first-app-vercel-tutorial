917c4b380b3ae23181e90f3897d2ebc7e38a0aa6
Joy Serquiña <stepjoyous@gmail.com>
2023-03-31 21:20:02 -0700

    Added profile.jpg image to public folder to be used later. Added Head component to first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,16 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function FirstPost() {
   return (
-    <main>
-      <h1>First post!</h1>
-      <Link href='/' name='Go back to home page'>Go back Home</Link>
-    </main>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <main>
+        <h1>First post!</h1>
+        <Link href='/' name='Go back to home page'>Go back Home</Link>
+      </main>
+    </>
   )
 }
