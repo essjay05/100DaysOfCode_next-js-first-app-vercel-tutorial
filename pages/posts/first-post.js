75253067f139b177745edc9c38d799ccab5c5c4f
Joy Serquiña <stepjoyous@gmail.com>
2023-03-31 21:55:03 -0700

    Created components folder with Layout component. Installed sass. Added styles.container to Layout.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
+
+import Layout from '../../components/Layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <main>
         <h1>First post!</h1>
         <Link href='/' name='Go back to home page'>Go back Home</Link>
       </main>
-    </>
+    </Layout>
   )
 }
