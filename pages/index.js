e88765f7f6a63b258b93415e81383070bcf018c2
Joy Serquiña <stepjoyous@gmail.com>
2023-04-01 10:07:57 -0700

    Added HeadingSm style. Swapped getStaticProps to ping my projects api.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,62 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-import { getSortedPostsData } from '../lib/posts';
+// import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  // const allPostsData = await getSortedPostsData();
+  // return {
+  //   props: {
+  //     allPostsData,
+  //   },
+  // };
+  const allProjectsData = await fetch('https://find-your-joy-app.onrender.com/api/projects')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      const apiProjectsData = data
+      console.log('fetch apiProjectsData')
+      console.log(apiProjectsData)
+      return apiProjectsData.data
+    })
+    .catch(err => console.error(err))
   return {
     props: {
-      allPostsData,
+      allProjectsData,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export async function getApiStaticProps() {
+  
+}
+
+export default function Home({ allProjectsData }) {
+  
+  // const [ projects, setProjects ] = useState(null)
+
+  // console.log('projects')
+  // console.log(projects)
+
+  // useEffect(() => {
+  //   const apiResults = getApiStaticProps()
+  //   setProjects(apiResults)
+  //   console.log('apiResults in useEffect')
+  //   console.log(apiResults)
+  // }, [])
+
+  console.log('allProjectsData')
+  console.log(allProjectsData)
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <Link href='/posts/first-post'>Go to First Post Page</Link>
       <section className={utilStyles.headingMd}>
         <p>I'm a Joy-of-All-Trades former theatre stage manager turned software engineer. You can find my contact info on my <a href='https://www.joyserquina.com' target='_blank'>portfolio</a>.</p>
         <br/>
@@ -27,7 +65,22 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      { allProjectsData.length > 0 ?
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Projects</h2>
+          <ul className={utilStyles.list}>
+            {allProjectsData.map(({ id, attributes}) => {
+              <li className={utilStyles.listItem} key={`project-${id}`}>
+                <h3 className={utilStyles.headingSm}>{attributes.name}</h3>
+                <p>{attributes.description}</p>
+              </li>
+            })}
+          </ul>
+        </section>
+      :
+        <></>
+      }
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -40,7 +93,7 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   );
 }
