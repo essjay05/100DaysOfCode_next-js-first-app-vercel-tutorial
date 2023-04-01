5be989e48664ff5075c438b4828e8bf8f168cc8e
Joy Serquiña <stepjoyous@gmail.com>
2023-03-31 20:31:19 -0700

    Implemented Link next component to both index.js and first-post.js to enable routing.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-  return <h1>First post!</h1>
+  return (
+    <main>
+      <h1>First post!</h1>
+      <Link href='/' name='Go back to home page'>Go back Home</Link>
+    </main>
+  )
 }
